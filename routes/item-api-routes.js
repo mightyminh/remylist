9ecf9a221f57d8a5234153cb6f626b808191d582
Erin <eringlabe@gmail.com>
2017-05-03 21:10:40 -0700

    api lend hard values

--- routes/item-api-routes.js
+++ routes/item-api-routes.js
@@ -1,3 +1,19 @@
 var db = require("../models");
 
-module.exports = function(app) {};
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+    // POST a new item to lend
+    app.post("/api/lend", function(req, res) {
+        db.Item.create({
+            name: "sewing maching",
+            category: "Homegoods",
+            description: "simple sewing machine",
+            imageURL: "https://images-na.ssl-images-amazon.com/images/I/6183wgXfS5L._SL1500_.jpg",
+            UserId: 1
+        }).then(function(dbItem) {
+            res.json(dbItem);
+        });
+    });
+};
