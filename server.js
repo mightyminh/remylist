8ea1b3e8713777a179afae5dade83db32ee70da8
Erin <eringlabe@gmail.com>
2017-05-03 20:08:48 -0700

    route files

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-// app.use(express.static("./public"));
+app.use(express.static("./public"));
 
 // Routes =============================================================
 
-// require("./routes/html-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/item-api-routes.js")(app);
+require("./routes/user-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
