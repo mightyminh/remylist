aa6b9ffd87ae611cef755fe96baebcf06f0ae2fb
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-05-03 21:07:45 -0700

    User API routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,3 +1,41 @@
 var db = require("../models");
+var userDataId = 1;
 
-module.exports = function(app) {};
+module.exports = function(app) {
+
+    // Logged-in user details
+    app.get("/api/profile", function(req, res) {
+        db.User.findAll({
+            where: {
+                id: userDataId
+            }
+        }).then(function(dbGet) {
+            res.json(dbGet);
+        });
+    });
+
+    // Sign-up new user
+    app.post("/api/signup", function(req, res) {
+        db.User.create({
+            userName: "radhika",
+            password: "radhika",
+            fullName: "radhika s",
+            email: "radhika @gmail.com",
+            location: "escondido"
+        }).then(function(dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    // Logged-in user lend items
+    app.get("/api/lend", function(req, res) {
+        db.Item.findAll({
+            where: {
+                UserId: userDataId
+            },
+            include: [db.User]
+        }).then(function(dbGet) {
+            res.json(dbGet);
+        });
+    });
+};
