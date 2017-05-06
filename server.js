06606abe5326ec4a65e5bf243dae208e08517638
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-05-06 10:24:25 -0700

    Server js file

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
+
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var passport = require('passport');
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -25,7 +21,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory
 app.use(express.static("./public"));
 
-// Routes =============================================================
+// Routes
 require("./routes/html-routes.js")(app);
 require("./routes/item-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app, passport);
