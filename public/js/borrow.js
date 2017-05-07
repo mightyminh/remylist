282b675408dec2addfe4464412f9f71c3548e344
Yousrat <ybanna@gmail.com>
2017-05-07 14:00:45 -0700

    borrow.js_final_show all

--- public/js/borrow.js
+++ public/js/borrow.js
@@ -13,15 +13,17 @@ $(document).ready(function() {
 
     function renderAll(data) {
         var allData = data;
-        // if (!allData || !allData.length) {
-        //     // $(".info-display").text("record not available");
-        // } else {   
-        // }
-        $("#item-name").text("Item: " + allData[0].name);
-        $("#item-desc").text("Description: " + allData[0].description);
-        $("#category").text("Category: " + allData[0].category);
-        $("#img").text("Category: " + allData[0].imageURL);
-        $("#avail").text("Available: " + allData[0].available);
+        if (!allData || !allData.length) {
+            $(".all-items").text("record not available");
+        } else {
+            for (var i = 0; i < allData.length; i++) {
+                $("#item-name").append("Item: " + allData[i].name + "  |  ");
+                $("#item-desc").append("Description: " + allData[i].description + "  |  ");
+                $("#category").append("Category: " + allData[i].category + "  |  ");
+                $("#img").append("Category: " + allData[i].imageURL + "  |  ");
+                $("#avail").append("Available: " + allData[i].available + "  |  ");
+            }
+        }
 
 
 
