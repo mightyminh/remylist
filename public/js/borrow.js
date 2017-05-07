a8fff0aa4c198214d75846210dc28741bcd48780
Yousrat <ybanna@gmail.com>
2017-05-07 13:51:04 -0700

    borrow.js

--- public/js/borrow.js
+++ public/js/borrow.js
@@ -6,10 +6,9 @@ $(document).ready(function() {
     }
 
     function getAllData() {
-       $('.all').on('click', function() {
+        $('.all').on('click', function() {
             getAll();
-            // use userData["0"].id 
-        }); 
+        });
     }
 
     function renderAll(data) {
@@ -17,8 +16,15 @@ $(document).ready(function() {
         // if (!allData || !allData.length) {
         //     // $(".info-display").text("record not available");
         // } else {   
-
         // }
+        $("#item-name").text("Item: " + allData[0].name);
+        $("#item-desc").text("Description: " + allData[0].description);
+        $("#category").text("Category: " + allData[0].category);
+        $("#img").text("Category: " + allData[0].imageURL);
+        $("#avail").text("Available: " + allData[0].available);
+
+
+
         console.log(allData);
     }
 
