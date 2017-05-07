b136d97b53042f4a8304febc91a7675fb3f72c42
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-05-07 12:12:25 -0700

    DB sync changed

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ require("./routes/item-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app, passport);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
