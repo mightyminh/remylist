ea28a0b94eb5f88517a54fa0ac591dd93d688d19
Yousrat <ybanna@gmail.com>
2017-05-07 15:53:00 -0700

    updates

--- public/js/borrow.js
+++ public/js/borrow.js
@@ -5,32 +5,56 @@ $(document).ready(function() {
         $.get('/api/allItems', renderAll);
     }
 
-    function getAllData() {
+    function getCategory1() {
+        $.get('/api/itemsCategory', renderCategory1);
+    }
+
+    function getData() {
         $('.all').on('click', function() {
             getAll();
+        });
+        $('.dropdown-menu .category1').on('click', function() {
+            getCategory1();
         });
     }
 
     function renderAll(data) {
         var allData = data;
         if (!allData || !allData.length) {
-            $(".all-items").text("record not available");
+            $(".items").text("record not available");
         } else {
             for (var i = 0; i < allData.length; i++) {
                 $("#item-name").append("Item: " + allData[i].name + "  |  ");
                 $("#item-desc").append("Description: " + allData[i].description + "  |  ");
                 $("#category").append("Category: " + allData[i].category + "  |  ");
-                $("#img").append("Category: " + allData[i].imageURL + "  |  ");
+                $("#img").append("Image URL: " + allData[i].imageURL + "  |  ");
                 $("#avail").append("Available: " + allData[i].available + "  |  ");
             }
         }
-
-
-
         console.log(allData);
+
     }
 
-    getAllData();
+    function renderCategory1(data) {
+        var dataByCategory = data;
+        if (!dataByCategory || !dataByCategory.length) {
+            $(".items").text("record not available");
+        } else {
+            for (var i = 0; i < dataByCategory.length; i++) {
+                $("#item-name").append("Item: " + dataByCategory[i].name + "  |  ");
+                $("#item-desc").append("Description: " + dataByCategory[i].description + "  |  ");
+                $("#img").append("Image URL: " + dataByCategory[i].imageURL + "  |  ");
+                $("#avail").append("Available: " + dataByCategory[i].available + "  |  ");
+
+            }
+
+        }
+
+        console.log(dataByCategory);
+
+    }
+
+    getData();
 });
 
 
