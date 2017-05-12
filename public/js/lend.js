c2c505118e6b543821e99e211291fcc207ce84fe
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-05-11 20:34:08 -0700

    Strict sequetial execution of actions corected. (changed id to class).

--- public/js/lend.js
+++ public/js/lend.js
@@ -1,63 +1,91 @@
 $(document).ready(function() {
 
-    // Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}      
-
-    function getLendInfo() {
-        $.get('/api/lend', renderLendInfo);
-    }
-
-     function postLendInfo() {
-        $.post('/api/lend', sendLendInfo);
-    }
-
- function renderLendInfo(data) {
-        var userLendData = data;
-        // if (!userLendData || !userLendData.length) {
-        //     $(".info-display").text("record not available");
-        // } else {
-
-            // $("#item-name").append("Item: " + userLendData[i].name + "  |  ");
-            // $("#item-desc").append("Description: " + userLendData[i].description + "  |  ");
-            // $("#category").append("Category: " + userLendData[i].category + "  |  ");
-            // $("#img").append("Image URL: " + userLendData[i].imageURL + "  |  ");
-            // $("#avail").append("Available: " + userLendData[i].available + "  |  ");
-        // }
-        console.log(userLendData);
-    }
-
-
-function sendLendInfo() {
-        // if (!userLendData || !userLendData.length) {
-        //     $(".info-display").text("record not available");
-        // } else {
-
-            // $("#item-name").append("Item: " + userLendData[i].name + "  |  ");
-            // $("#item-desc").append("Description: " + userLendData[i].description + "  |  ");
-            // $("#category").append("Category: " + userLendData[i].category + "  |  ");
-            // $("#img").append("Image URL: " + userLendData[i].imageURL + "  |  ");
-            // $("#avail").append("Available: " + userLendData[i].available + "  |  ");
-        // }
-        console.log(userLendData);
-    }
-
-function NewLendInfo(){
-    $('.addButton').on('click', function() {
-            postLendInfo(name,description,imgUrl,category);
+    $(".make-unavailable").on("click", function(e) {
+        console.log(e.target);
+        var itemUnavailable = {
+            itemId: $(this).attr("available-item-id")
+        };
+        $.ajax({
+            method: "PUT",
+            url: "/item-unavailable",
+            data: itemUnavailable
+        }).done(function() {
+            window.location.href = "/items-by-lender";
         });
-}
+    });
 
-    getLendInfo();
-    NewLendInfo();
+    $(".make-available").on("click", function() {
+        var itemAvailable = {
+            itemId: $(this).attr("unavailable-item-id")
+        };
+        $.ajax({
+            method: "PUT",
+            url: "/item-available",
+            data: itemAvailable
+        }).done(function() {
+            window.location.href = "/items-by-lender";
+        });
+    });
 
+    $("#add-new-item").on("click", function() {
+        var newItem = {
+            addItemName: $("#add-item-name").val().trim(),
+            addItemDescription: $("#add-item-description").val().trim(),
+            addItemUrl: $("#add-item-url").val().trim(),
+            addItemCategory: $("#add-item-category").val().trim(),
+            addItemLender: $("#add-new-item").attr("lender-id")
+        };
+        $.ajax({
+            method: "POST",
+            url: "/add-item",
+            data: newItem
+        }).done(function() {
+            window.location.href = "/items-by-lender";
+        });
+    });
+
+    $(".delete-item").on("click", function() {
+        var deleteId = {
+            itemId: $(".delete-item").attr("delete-item-id")
+        };
+
+        $.ajax({
+            method: "DELETE",
+            url: "/delete-item",
+            data: deleteId
+        }).done(function() {
+            window.location.href = "/items-by-lender";
+        });
+    });
+
+    var itemRequestId;
+
+    $(".request-item").on("click", function() {
+        var request = {
+            itemId: $(this).attr("request-item-id")
+        };
+        itemRequestId = request.itemId;
+        $.ajax({
+            method: "PUT",
+            url: "/set-borrower",
+            data: request
+        }).done(function() {
+            window.location.href = "/all-items";
+        });
+    });
+
+    $(".send-mail").on("click", function() {
+        var mail = {
+            numDays: $("#num-days").val(),
+            mailMessage: $("#email-message").val(),
+            itemId: itemRequestId
+        };
+        $.ajax({
+            method: "POST",
+            url: "/send-mail",
+            data: mail
+        }).done(function() {
+            window.location.href = "/all-items";
+        });
+    });
 });
-
-        
-  
